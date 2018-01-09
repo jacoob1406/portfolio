9649e4cc71631d02f5f9ccd647dfa92b6d76854a
Jakub <lemka.jakub@gmail.com>
2018-01-09 17:35:51 +0100

    menu links and more

--- js/script.js
+++ js/script.js
@@ -50,29 +50,4 @@ window.onload = function () {
         window.scroll({top: h + 1, left: 0, behavior: 'smooth'});
     };
 
-    function smoothScroll(event) {
-        if (event.target.hash !== '') { //Check if tag is an anchor
-            event.preventDefault()
-            const hash = event.target.hash.replace("#", "")
-            const link = document.getElementsByName(hash)
-            //Find the where you want to scroll
-            const position = link[0].getBoundingClientRect().y
-            let top = 0
-
-            let smooth = setInterval(() => {
-                let leftover = position - top
-                if (top === position) {
-                    clearInterval(smooth)
-                } else if (position > top && leftover < 10) {
-                    top += leftover
-                    window.scrollTo(0, top)
-                } else if (position > (top - 10)) {
-                    top += 10
-                    window.scrollTo(0, top)
-                }
-
-            }, 6)//6 milliseconds is the faster chrome runs setInterval
-        }
-    }
-
 };
