a2b7046b6b45833eff5d8cb9699f920ec703de6b
Jakub <lemka.jakub@gmail.com>
2018-01-11 15:22:59 +0100

    contact form configured

--- js/script.js
+++ js/script.js
@@ -1,43 +1,6 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 window.onload = function () {
 
-//    if (!!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g))
-//    {
-//
-//    }
-//FUNKCJA KTÓRA SKRACA W CAŁYM DOKUMENCIE DOC.GETELEMBYID
-    function _(id) {
-        return document.getElementById(id);
-    }
-    function submitForm() {
-        _('submit').disabled = true;
-        _('status').innerHTML = 'please wait ...';
-        var formdata = new FormData();
-        formdata.append("name", _("name").value);
-        formdata.append("email", _("email").value);
-        formdata.append("message", _("message").value);
-        var ajax = new XMLHttpRequest();
-        ajax.open("POST", "../php/mail.php");  //?? czy znajdzie nam plik php ?
-        ajax.onreadystatechange = function () {
-            if (ajax.readyState == 4 && ajax.status == 200) {
-                if (ajax.responseText == "success") {
-                    _("myform").innerHTML = '<h2>Thanks ' + _("name").value + ', your message has been sent.</h2>';
-                } else {
-                    _("status").innerHTML = ajax.responseText;
-                    _("submit").disabled = false;
-                }
-            }
-        }
-        ajax.send(formdata);
-    }
-
-//----------------------------------------------------------------
-
-    window.addEventListener('scroll', function () {
+    function coverFirst() {
         if (window.innerWidth > 768) {
             let ypos = window.pageYOffset;
             let welc = document.getElementsByClassName('welcome')[0];
@@ -45,40 +8,9 @@ window.onload = function () {
         } else {
             return false;
         }
-    })
-
-
-
-    document.getElementById('open').onclick = showMenu;
-    function showMenu() {
-        document.getElementById('main-heading').style.display = 'none';
-        document.getElementById('overlay-menu').style.height = 'auto';
-        document.getElementById('overlay-menu').style.opacity = '1';
-        document.getElementsByClassName('welcome')[0].style.background = '#A9A9A9';
-        this.style.opacity = '0';
-        this.style.display = 'none';
-        document.getElementById('close').style.display = 'block';
-        document.getElementById('close').style.opacity = '1';
     }
 
-    document.getElementById('close').onclick = hideMenu;
-
-    function hideMenu() {
-        document.getElementById('overlay-menu').style.height = '0';
-        document.getElementById('overlay-menu').style.opacity = '0';
-        document.getElementsByClassName('welcome')[0].style.background = '#FF3B3F';
-        document.getElementById('main-heading').style.display = 'block';
-        this.style.opacity = '0';
-        this.style.display = 'none';
-        document.getElementById('open').style.display = 'block';
-        document.getElementById('open').style.opacity = '1';
-    }
-
-    document.getElementById('stick-menu').onclick = function () {
-        document.getElementById('menu-list').classList.toggle('hideul');
-    };
-
-    window.onscroll = function () {
+    function toggleMenu() {
         const menu = document.getElementById('menu-items');
         const h = document.getElementsByClassName('welcome')[0].offsetHeight;
         if (window.pageYOffset > h) {
@@ -86,12 +18,9 @@ window.onload = function () {
         } else {
             menu.classList.remove("stick");
         }
-        ;
-    };
+    }
 
-//cv dpwnload
     var disable = true;
-
     function slideCv() {
         const cv = document.getElementById('cv');
         let h = document.getElementsByClassName('welcome')[0].offsetHeight
@@ -105,7 +34,42 @@ window.onload = function () {
         }
     }
 
-    window.onscroll = slideCv;
+    window.addEventListener('scroll', slideCv);
+    window.addEventListener('scroll', toggleMenu);
+    window.addEventListener('scroll', coverFirst);
+
+    function showMenu() {
+        document.getElementById('main-heading').style.display = 'none';
+        document.getElementById('overlay-menu').style.height = 'auto';
+        document.getElementById('overlay-menu').style.opacity = '1';
+        document.getElementsByClassName('welcome')[0].style.background = '#A9A9A9';
+        this.style.opacity = '0';
+        this.style.display = 'none';
+        document.getElementById('close').style.display = 'block';
+        document.getElementById('close').style.opacity = '1';
+    }
+
+    function hideMenu() {
+        document.getElementById('overlay-menu').style.height = '0';
+        document.getElementById('overlay-menu').style.opacity = '0';
+        document.getElementsByClassName('welcome')[0].style.background = '#FF3B3F';
+        document.getElementById('main-heading').style.display = 'block';
+        this.style.opacity = '0';
+        this.style.display = 'none';
+        document.getElementById('open').style.display = 'block';
+        document.getElementById('open').style.opacity = '1';
+    }
+
+    document.getElementById('open').onclick = showMenu;
+    document.getElementById('close').onclick = hideMenu;
+
+    document.getElementById('stick-menu').onclick = function () {
+        document.getElementById('menu-list').classList.toggle('hideul');
+    };
+
+    document.getElementById('menu-list').onclick = function () {
+        this.classList.toggle('hideul');
+    };
 
     document.getElementById('cvclose').onclick = function () {
         document.getElementById('cv').classList.remove("showcv");
@@ -126,5 +90,4 @@ window.onload = function () {
         document.getElementById('video').style.opacity = 0;
         document.getElementById('video').style.display = 'none';
     };
-
 };
